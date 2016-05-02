65ae7f12955bb939d9c4bf5331daa3eba205d3a4
Jesla <jesslar415@gmail.com>
2016-05-02 10:35:49 -0500

    depotulab-angularcalc

---
+++ controllers.js
@@ -0,0 +1,8 @@
+var controllers = angular.module("angularCalculator", []);
+controllers.controller("angularCalcController", function ($scope) {
+    $scope.firstObjective = function () {
+        alert("One Down! Five to Go!")
+    };
+
+        
+});
